6db26f2557881f06d7aaf3b23e4fec09f5756194
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2020-03-23 10:07:11 +0100

    Update Flex server image

--- symfony-flex-server/run.sh
+++ symfony-flex-server/run.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ "$1" == "run" ]]; then
+    shift
+fi
+PARAMS=$@
+
+echo Init with $PARAMS
 
 cd /var/www/flex && \
-    php bin/console recipe:init private
+    php bin/console recipe:init $PARAMS
 
 /usr/local/bin/apache2-foreground
 
